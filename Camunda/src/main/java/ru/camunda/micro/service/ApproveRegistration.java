977ff4b47e4eaba55bd4a5dfbbf86c53e2d946ea
Aleksandr Lopatin <lav_91@bk.ru>
2022-12-04 12:24:58 +0400

    ReadMe

--- Camunda/src/main/java/ru/camunda/micro/service/ApproveRegistration.java
+++ Camunda/src/main/java/ru/camunda/micro/service/ApproveRegistration.java
@@ -2,11 +2,12 @@ package ru.camunda.micro.service;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 import ru.camunda.micro.controller.feign.RegistrationSender;
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApproveRegistration implements JavaDelegate {
@@ -14,14 +15,13 @@ public class ApproveRegistration implements JavaDelegate {
 
     private final RegistrationSender registrationSender;
 
-    private final String SUCCESS_REGISTRATION_MESSAGE = "[%s] User %s has been successfully registered";
+    private final String SUCCESS_REGISTRATION_MESSAGE = "User {} has been successfully registered";
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
 
-        System.out.println(String.format(SUCCESS_REGISTRATION_MESSAGE,
-                ApproveRegistration.class.getName(),
-                delegateExecution.getVariable("username")));
+        log.info(SUCCESS_REGISTRATION_MESSAGE,delegateExecution.getVariable("username"));
+
 
         Integer id = (Integer) delegateExecution.getVariable("userId");
 
